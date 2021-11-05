50c6313dabedf7af71694137af4c1471c6701d49
42seouler <77034008+42seouler@users.noreply.github.com>
2021-11-06 01:56:12 +0900

    feature : user unit-test
    - 올바른 값에 따른 테스트
    - 컨트롤러 테스트
    - 서비스 테스트

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,10 @@
-import { BadRequestException, HttpException, Injectable, NotFoundException, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -8,37 +14,35 @@ import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-
   constructor(
     @InjectRepository(Users)
     private repo: Repository<Users>,
-
-    @InjectRepository(UsersRepository)
-    private usersCustomRepo: UsersRepository
-  ) { }
+  ) // @InjectRepository(UsersRepository)
+  // private usersCustomRepo: UsersRepository
+  {}
 
   findAll() {
     return this.repo.find();
   }
 
   async findOne(id: string) {
-    const user = await this.repo.findOne(id)
+    const user = await this.repo.findOne(id);
     if (!user) throw new NotFoundException(`User #${id} not found`);
     return user;
   }
   async findOneQuery(id: string) {
-    const user = await this.usersCustomRepo.findOne(id)
+    const user = await this.repo.findOne(id);
     if (!user) throw new NotFoundException(`User #${id} not found`);
     return user;
   }
   async findByEmail(email: string) {
-    const user = await this.repo.findOne({ email })
+    const user = await this.repo.findOne({ email });
     if (!user) throw new NotFoundException(`User with #${email} was not found`);
     return user;
   }
 
   async create(createUserDto: CreateUserDto) {
-    const user = await this.repo.findOne({ email: createUserDto.email })
+    const user = await this.repo.findOne(createUserDto.email)
     if (user) throw new BadRequestException('Email already in use');
 
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
@@ -52,7 +56,7 @@ export class UsersService {
 
   async update(id: string, attrs: Partial<Users>) {
     const user = await this.findOne(id);
-    if (!user) throw new NotFoundException('User not found')
+    if (!user) throw new NotFoundException('User not found');
 
     Object.assign(user, attrs);
     // Using save instead of update to make use of hooks
