faa7db04d7f78e35888e1610159743e8a93a28e1
chunu-j <codehouse9@naver.com>
2021-11-05 13:52:13 +0900

    Feat: menus CRUD 구현

--- src/menus/enums/menu-category.enum.ts
+++ src/menus/enums/menu-category.enum.ts
@@ -1,12 +1,12 @@
 export enum MenuCategory {
-  SALAD = 'SALAD',
-  SUBSCRIPTION = 'SUBSCRIPTION',
-  SALADWRAP = 'SALAD-WRAP',
-  MEAL = 'MEAL',
-  SOUP = 'SOUP',
-  SET = 'SET',
-  SNACK = 'SNACK',
-  DRINK = 'DRINK',
-  EVENT = 'EVENT',
-  SOON = 'SOON',
+  SALAD = 'SALAD', // 샐러드
+  SUBSCRIPTION = 'SUBSCRIPTION', // 정기구독
+  SALADWRAP = 'SALAD-WRAP', // 랩, 샌드위치
+  MEAL = 'MEAL', // 도시락, 간편식
+  SOUP = 'SOUP', // 죽, 스프
+  SET = 'SET', // 세트상품
+  SNACK = 'SNACK', // 간식
+  DRINK = 'DRINK', // 음료
+  EVENT = 'EVENT', // 장건강 필수템
+  SOON = 'SOON', // 오픈예정
 }
--- src/menus/menus.service.ts
+++ src/menus/menus.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMenuDto } from './dto/create-menu.dto';
+import { UpdateMenuDto } from './dto/update-menu.dto';
+import { Menu } from './entities/menu.entity';
 
 @Injectable()
-export class MenusService {}
+export class MenusService {
+  private menus: Menu[] = [];
+  create(createMenuDto: CreateMenuDto) {
+    this.menus.push({
+      id: this.menus.length + 1,
+      ...createMenuDto,
+    });
+    return 'This action adds a new menu';
+  }
+
+  findAll(): Menu[] {
+    return this.menus;
+  }
+
+  findOne(id: number): Menu {
+    const menu = this.menus.find((menu) => menu.id === id);
+    if (!menu) {
+      throw new NotFoundException(`Menu with ID: ${id} not found`);
+    }
+    return menu;
+  }
+
+  update(id: number, updateMenuDto: UpdateMenuDto) {
+    const menu = this.findOne(id);
+    this.remove(id);
+    this.menus.push({ ...menu, ...updateMenuDto });
+    return `This action updates a #${id} menu`;
+  }
+
+  remove(id: number): boolean {
+    this.findOne(id);
+    this.menus = this.menus.filter((menu) => menu.id !== id);
+    return true;
+  }
+}
