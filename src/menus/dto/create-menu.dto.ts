a8439b35ce6f7c18c30862343f1c835ee8d8feed
Eugene Choi <42341135+n12seconds@users.noreply.github.com>
2021-11-06 01:58:02 +0900

    Feat: item, menu, tag dto에 swagger api property 추가

--- src/menus/dto/create-menu.dto.ts
+++ src/menus/dto/create-menu.dto.ts
@@ -1,14 +1,26 @@
 import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMenuDto {
+
   @IsString()
+  @ApiProperty()
   readonly category: string; //MenuCategory;
+
   @IsString()
+  @ApiProperty()
   readonly name: string;
+
   @IsString()
+  @ApiProperty()
   readonly description: string;
+
   @IsBoolean()
+  @ApiProperty()
   readonly isSold: boolean;
+
   @IsString()
+  @ApiProperty()
   readonly badge: string;
+  
 }
--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -2,12 +2,19 @@ import { Column, ManyToOne } from 'typeorm';
 import { TagType } from '../enums/tag-type.enum';
 import { Menu } from '../../menus/entities/menu.entity';
 import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTagDto {
+
   @IsNumber()
+  @ApiProperty()
   readonly menuId: number;
+
   @IsString()
+  @ApiProperty()
   readonly type: string; //TagType;
+  
   @IsString()
+  @ApiProperty()
   readonly name: string; //Menu;
 }
