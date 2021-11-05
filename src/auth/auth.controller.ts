e2bee90ab83fa495e42a88a7faa4d04b803babb1
Eugene Choi <42341135+n12seconds@users.noreply.github.com>
2021-11-06 00:37:02 +0900

    feat: item, menu, tag swagger 설정 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('회원 API')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   async login(@Request() req) {
     console.log('req', req)
     return this.authService.login(req);
   }
 
+  @ApiOperation({ summary: '프로필 조회' })
   @Get('profile')
   @UseGuards(JwtAuthGuard)
   getProfile(@Request() req) {
--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -16,34 +16,41 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RolesGuard } from 'src/users/guards/roles.guard';
 import { Roles } from 'src/users/decorators/roles.decorator';
 import { Role } from 'src/users/roles.types';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('상품 태그 API')
 @Controller('tags')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class TagsController {
   constructor(private readonly tagsService: TagsService) { }
 
+  @ApiOperation({ summary: '태그 등록' })
   @Post()
   @Roles(Role.Admin)
   create(@Body() createTagDto: CreateTagDto) {
     return this.tagsService.create(createTagDto);
   }
 
+  @ApiOperation({ summary: '태그 리스트 조회' })
   @Get()
   findAll(@Query() query) {
     return this.tagsService.findAll(query);
   }
 
+  @ApiOperation({ summary: '태그 단건 조회' })
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.tagsService.findOne(id);
   }
 
+  @ApiOperation({ summary: '태그 수정' })
   @Patch(':id')
   @Roles(Role.Admin)
   update(@Param('id') id: number, @Body() updateTagDto: UpdateTagDto) {
     return this.tagsService.update(id, updateTagDto);
   }
 
+  @ApiOperation({ summary: '태그 삭제' })
   @Delete(':id')
   @Roles(Role.Admin)
   remove(@Param('id') id: number) {
